928e5c916632b238e310f2dda3e6eba4741f6ad6
dkyoung2004 <dkyoung0328@gmail.com>
2021-09-11 11:58:09 +0900

    update

--- NN_CODE/gate_code.py
+++ NN_CODE/gate_code.py
@@ -1,18 +1,30 @@
-def AND_gate(x1,x2):
-    w1=0.7
-    w2=0.7
-    b=-0.4
-    result = x1*w1+x2*w2 +b
-    if result >=1:
-        return 1
-    else:
-        return 0
-def NAND_gate(x1,x2):
-    w1=0.7
-    w2=0.7
-    b=-0.4
-    result = x1*w1+x2*w2 +b
-    if result >= 1:
+def AND_gate(x1, x2):
+    w1=0.5
+    w2=0.5
+    b=-0.7
+    result = x1*w1 + x2*w2 + b
+    if result <= 0:
         return 0
     else:
         return 1
+def NAND_gate(x1, x2):
+    w1=-0.5
+    w2=-0.5
+    b=0.7
+    result = x1*w1 + x2*w2 + b
+    if result <= 0:
+        return 0
+    else:
+        return 1
+def OR_gate(x1, x2):
+    w1=0.6
+    w2=0.6
+    b=-0.5
+    result = x1*w1 + x2*w2 + b
+    if result <= 0:
+        return 0
+    else:
+        return 1
+print('AND_gate: (0,0,{}) (0,1,{}) (1,0,{}) (1,1,{})'.format(AND_gate(0,0),AND_gate(0,1),AND_gate(1,0),AND_gate(1,1)))
+print('NAND_gate: (0,0,{}) (0,1,{}) (1,0,{}) (1,1,{})'.format(NAND_gate(0,0),NAND_gate(0,1),NAND_gate(1,0),NAND_gate(1,1)))
+print('OR_gate: (0,0,{}) (0,1,{}) (1,0,{}) (1,1,{})'.format(OR_gate(0,0),OR_gate(0,1),OR_gate(1,0),OR_gate(1,1)))
