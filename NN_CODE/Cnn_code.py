15e8f53b47bc8d4dca8d0fe7b180c75e6c893f35
dkyoung2004 <64358951+dkyoung2004@users.noreply.github.com>
2023-06-12 19:19:51 +0900

    Update Cnn_code.py

--- NN_CODE/Cnn_code.py
+++ NN_CODE/Cnn_code.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 print("Tensorflow version:",tf.__version__)
 ## 텐서플로우 라이브러리 import 후 버전확인
 mnist = tf.keras.datasets.mnist
-
 (x_train, y_train),(x_test,y_test) = mnist.load_data()
 
 x_train, x_test = x_train/ 255.0, x_test/255.0
